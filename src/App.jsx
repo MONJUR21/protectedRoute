1080b4673ff1199a911cba3940d1838267b2b321
MONJUR RAHAMAN STUDENT <monjur.csecu@gmail.com>
2024-10-19 14:27:12 +0600

    edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate, Link } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes, Navigate, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes,Navigate,Link} from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Login from './components/Login';
